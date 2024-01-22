7204c51c280d9595e746087a837bc1932278196d
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-01-22 14:41:57 +0900

    Add load method to TokenInfoRepository

--- spotify_api/domain/infrastructure/token_info_repository.py
+++ spotify_api/domain/infrastructure/token_info_repository.py
@@ -1,4 +1,5 @@
 from abc import ABCMeta, abstractmethod
+from typing import Optional
 
 class TokenInfoRepository(metaclass=ABCMeta):
     """
@@ -9,5 +10,12 @@ class TokenInfoRepository(metaclass=ABCMeta):
     def save(self, token_info: dict) -> bool:
         """
         トークン情報を保存する
+        """
+        pass
+
+    @abstractmethod
+    def load(self) -> Optional[dict]:
+        """
+        トークン情報を取得する
         """
         pass
