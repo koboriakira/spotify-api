6954a6cd1760e3fa4878f13553b064dbe47102cd
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-01-22 14:09:35 +0900

    Refactor authorize module and add token info repository

--- spotify_api/interface/authorize.py
+++ spotify_api/interface/authorize.py
@@ -1,10 +1,12 @@
 from typing import Optional
 from usecase.authorize_usecase import AuthorizeUsecase
+from infrastructure.token_info_s3_repository import TokenInfoS3Repository
+
+repository = TokenInfoS3Repository()
+authorizeUsecase = AuthorizeUsecase(repository=repository)
 
 def get_authorize_url() -> str:
-    authorizeUsecase = AuthorizeUsecase()
     return authorizeUsecase.get_authorize_url()
 
 def authorize_callback(code: str) -> dict:
-    authorizeUsecase = AuthorizeUsecase()
     return authorizeUsecase.authorize_callback(code=code)
--- spotify_api/usecase/authorize_usecase.py
+++ spotify_api/usecase/authorize_usecase.py
@@ -1,10 +1,15 @@
 from typing import Optional
 from infrastructure.spotipy import Spotipy
 from custom_logger import get_logger
+from domain.infrastructure.token_info_repository import TokenInfoRepository
 
 logger = get_logger(__name__)
 
 class AuthorizeUsecase:
+    def __init__(self, repository: TokenInfoRepository):
+        self.repository = repository
+
+
     def get_authorize_url(self) -> str:
         authorize_url = Spotipy.get_authorize_url()
         logger.info("authorize_url: " + authorize_url)
@@ -12,4 +17,5 @@ class AuthorizeUsecase:
 
     def authorize_callback(self, code: str) -> dict:
         token_info = Spotipy.authorize_callback(code=code)
+        self.repository.save(token_info=token_info)
         return token_info
