a2a85c5d98cf1956accb47aa8efbacc188538fd1
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-01-22 14:57:38 +0900

    Refactor authorize_usecase.py to use SpotifyOauth instead of Spotipy

--- spotify_api/usecase/authorize_usecase.py
+++ spotify_api/usecase/authorize_usecase.py
@@ -1,5 +1,4 @@
-from typing import Optional
-from infrastructure.spotipy import Spotipy
+from infrastructure.spotify_oauth import SpotifyOauth
 from custom_logger import get_logger
 from domain.infrastructure.token_info_repository import TokenInfoRepository
 
@@ -8,14 +7,15 @@ logger = get_logger(__name__)
 class AuthorizeUsecase:
     def __init__(self, repository: TokenInfoRepository):
         self.repository = repository
+        self.spotify_oauth = SpotifyOauth()
 
 
     def get_authorize_url(self) -> str:
-        authorize_url = Spotipy.get_authorize_url()
+        authorize_url = self.spotify_oauth.get_authorize_url()
         logger.info("authorize_url: " + authorize_url)
         return authorize_url
 
     def authorize_callback(self, code: str) -> dict:
-        token_info = Spotipy.authorize_callback(code=code)
+        token_info = self.spotify_oauth.authorize_callback(code=code)
         self.repository.save(token_info=token_info)
         return token_info
